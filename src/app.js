c57b2719e8fb56c19a6edb69e1164e0582b66fc2
hoanghiep2625 <hoanghiep262005@gmail.com>
2025-06-25 01:30:38 +0700

    fixbug

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use(
       "http://localhost:5174",
       "http://localhost:5173",
       "https://elavia.tahoanghiep.com",
-      "https://admin.elavia.tahoanghiep.com/",
+      "https://admin.elavia.tahoanghiep.com",
     ],
     credentials: true,
   })
