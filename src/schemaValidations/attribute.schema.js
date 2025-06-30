49c5b79532a7d6c469ff66c841e25c3ad4ffc42c
hoanghiep2625 <hoanghiep262005@gmail.com>
2025-06-30 20:54:57 +0700

    fix bug

--- src/schemaValidations/attribute.schema.js
+++ src/schemaValidations/attribute.schema.js
@@ -2,7 +2,6 @@ import { z } from "zod";
 
 export const attributeSchema = z.object({
   name: z.string().min(2, "Tên thuộc tính cần tối thiểu 2 ký tự"),
-  slug: z.string().min(1, "Slug không được để trống"),
   values: z
     .array(z.string().min(1, "Giá trị không được để trống"))
     .min(1, "Cần ít nhất một giá trị"),
